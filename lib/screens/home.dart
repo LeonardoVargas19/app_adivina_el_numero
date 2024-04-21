83a539bd1b29b21db33b634b21038a838fb58f90
Leonardo Vargas Olvera <124732244+LeonardoVargas19@users.noreply.github.com>
2024-04-21 11:36:23 -0600

    Tablero
    Creacin del tablero el cual  contiene 3 containers uno que pedira un numero mayor otro menor por ultimo un historial de numero

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,50 +13,125 @@ class Home extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(top: 20),
         child: Column(children: [
+          _header(),
+          tablero(),
           Container(
             width: double.infinity,
-            height: 250,
-            color: Colors.red,
-            padding: EdgeInsets.only(top: 20, left: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            height: 100,
+            color: Colors.green,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 200,
-                      child: CupertinoTextField(
-                        keyboardType: TextInputType.number,
-                        placeholder: 'Número',
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    const Text(
-                      'Intentos \n 5',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                CupertinoButton(
-                    color: CupertinoColors.activeBlue,
-                    child: Text('Ingresar'),
-                    onPressed: () {}),
+                Expanded(
+                    child: Slider.adaptive(
+                  value: 0,
+                  max: 3,
+                  min: 0,
+                  label: 'test',
+                  onChanged: (value) {},
+                ))
               ],
             ),
           )
         ]),
+      ),
+    );
+  }
+}
+
+class tablero extends StatelessWidget {
+  const tablero({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+            height: 150,
+            child: Center(
+              child: Text(
+                'Ingrese un numero menor',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.yellow,
+            height: 150,
+            child: Center(
+                child: Text(
+              'Ingresa un numero menor',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            )),
+          ),
+        ),
+        Expanded(
+            child: Container(
+          color: Colors.purple,
+          height: 150,
+          child: Center(
+            child: Text('Historial'),
+          ),
+        ))
+      ],
+    );
+  }
+}
+
+class _header extends StatelessWidget {
+  const _header({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 250,
+      color: Colors.red,
+      padding: EdgeInsets.only(top: 20, left: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 200,
+                child: CupertinoTextField(
+                  keyboardType: TextInputType.number,
+                  placeholder: 'Número',
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              const Text(
+                'Intentos \n 5',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CupertinoButton(
+              color: CupertinoColors.activeBlue,
+              child: Text('Ingresar'),
+              onPressed: () {}),
+        ],
       ),
     );
   }
